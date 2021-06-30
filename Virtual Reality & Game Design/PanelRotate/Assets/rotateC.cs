ebce5d1701bc75e0a96cb752a8f2a7a19e1e9340
Sneezewort <86746723+Sneezewort@users.noreply.github.com>
2021-06-30 18:38:41 -0400

    Add files via upload

---
+++ Virtual Reality & Game Design/PanelRotate/Assets/rotateC.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rotateC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Button button;
+    public Transform transform;
+    public Text text;
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(clockwise);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void clockwise()
+    {
+        transform.Rotate(0f, 0f, -1f, Space.Self);
+        text.text = transform.rotation.eulerAngles.z + " Degrees";
+    }
+}
---
+++ Virtual Reality & Game Design/PanelRotate/Assets/rotateCC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rotateCC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Button button;
+    public Transform transform;
+    public Text text;
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(counterclockwise);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void counterclockwise()
+    {
+        transform.Rotate(0f, 0f, 1f, Space.Self);
+        text.text = transform.rotation.eulerAngles.z + " Degrees";
+    }
+}
