935b4a7252d1b4b72d5c2a4a403138c4523d3e80
Sneezewort <86746723+Sneezewort@users.noreply.github.com>
2021-06-30 18:52:15 -0400

    Add files via upload

---
+++ Virtual Reality & Game Design/TextPractice/Assets/CountDown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float time = 10f;
+    Text timer;
+    void Start()
+    {
+        timer = GetComponent<Text>();
+        timer.text = time.ToString("0.00") + "s";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time > 0f)
+            timer.text = time.ToString("0.00") + "s";
+        else
+            timer.text = "0s";
+    }
+}
