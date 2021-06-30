ff0f8301a6cb8f0b664ef229c65545b1d4fe95dc
Sneezewort <86746723+Sneezewort@users.noreply.github.com>
2021-06-30 18:05:16 -0400

    Add files via upload

---
+++ Virtual Reality & Game Design/AnimationProject/Assets/SpinCube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinCube : MonoBehaviour
+{
+
+    private Animation anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        anim["Animation"].speed = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (anim != null)
+                anim.Play("Animation");
+        }
+    }
+}
