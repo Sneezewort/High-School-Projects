fc38b0c4b837f9fa18064be62794e8a0f62fa224
Sneezewort <86746723+Sneezewort@users.noreply.github.com>
2021-06-30 19:03:39 -0400

    Add files via upload

---
+++ Virtual Reality & Game Design/MLAgents/MLAgents Example/Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class NewBehaviourScript : Agent
+{
+    public Transform cube;
+    private int count;
+    public override void OnEpisodeBegin()
+    {
+        count = 1200;
+        transform.localPosition = new Vector3(0, 0, 0);
+        cube.transform.localPosition = new Vector3(2, 0, 0);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        SetReward(1f);
+        EndEpisode();
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(cube.transform.localPosition);
+    }
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        transform.Translate(new Vector3(vectorAction[0] * Time.deltaTime, 0, vectorAction[1] * Time.deltaTime) * 3);
+        count--;
+        if (count <= 0)
+            EndEpisode();
+    }
+}
