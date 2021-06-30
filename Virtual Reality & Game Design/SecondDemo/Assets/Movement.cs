f142101ea64e15f115fd0c2615490fb4475d2e00
Sneezewort <86746723+Sneezewort@users.noreply.github.com>
2021-06-30 18:46:34 -0400

    Add files via upload

---
+++ Virtual Reality & Game Design/SecondDemo/Assets/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Debug.Log("Time passed: " + timer);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.up * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector2.down * Time.deltaTime);
+        transform.Translate(Vector3.forward * 0.5f*Time.deltaTime);
+
+    }
+}
