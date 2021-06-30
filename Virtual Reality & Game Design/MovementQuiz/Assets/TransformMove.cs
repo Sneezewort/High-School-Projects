6015f46da4aecc72149bc9da7f77f6fe03fac0c1
Sneezewort <86746723+Sneezewort@users.noreply.github.com>
2021-06-30 18:35:19 -0400

    Add files via upload

---
+++ Virtual Reality & Game Design/MovementQuiz/Assets/TransformMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Transform transform;
+    float speed = 5f;
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.back * Time.deltaTime * speed;
+        transform.Rotate(0, 100*Time.deltaTime, 0, Space.Self);
+    }
+}
---
+++ Virtual Reality & Game Design/MovementQuiz/Assets/TransformPhysics.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class TransformPhysics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    float force = 10f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var value = Input.GetAxis("Horizontal");
+        Debug.Log("Input - " + value);
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * -1);
+        rb.AddForce(new Vector3(0,0,1));
+        rb.AddForce(Vector3.forward * force * -1, ForceMode.Force);
+        rb.AddForceAtPosition(Vector3.forward * 2f * -1, new Vector3(0, 0 , 1), ForceMode.Force);
+    }
+}
