59c5e0c6983ceea597f157bf721043e59cff7e66
Sneezewort <86746723+Sneezewort@users.noreply.github.com>
2021-06-30 18:51:32 -0400

    Add files via upload

---
+++ Virtual Reality & Game Design/TextButton/Assets/ChangeText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Button button;
+    public Text text;
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TextChange);
+        //text = GameObject.Find("Canvas/Title").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void TextChange()
+    {
+        text.text = "Text Changed";
+    }
+}
